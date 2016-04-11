8795926176e147b835b3cf57f5ce953d2efaf09a
Luc Aebischer <aebiluc@gmail.com>
2016-04-11 11:16:08 +0200

    New redirection routehandler 	RouteHandlerRedirect : Allow redirection from the server.

---
+++ Sources/DotNet/Woopsa/HTTPServer/RouteHandlers/RouteHandelerRedirect.cs
@@ -0,0 +1,36 @@
+﻿namespace Woopsa
+{
+    public enum WoopsaRedirection
+    {
+        Temporary = 1,
+        Premanent = 2
+    }
+    public class RouteHandelerRedirect : IHTTPRouteHandler
+    {
+        public RouteHandelerRedirect(string location, WoopsaRedirection redirection)
+        {
+            _location = location;
+            _redirection = redirection;
+
+        }
+        public bool AcceptSubroutes
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void HandleRequest(HTTPRequest request, HTTPResponse response)
+        {
+            if(_redirection == WoopsaRedirection.Temporary)
+                response.SetStatusCode((int)HTTPStatusCode.TemporaryRedirect, "Temporary redirect");
+            else if(_redirection == WoopsaRedirection.Temporary)
+                response.SetStatusCode((int)HTTPStatusCode.Moved, "Permanent redirection");
+            response.SetHeader("Location", _location);
+        }
+
+        private string _location;
+        private WoopsaRedirection _redirection;
+    }
+}
