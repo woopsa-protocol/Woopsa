8447017a53e7b5162d63d60e80cab6168d12e509
Didier Mettler <didier.mettler@heig-vd.ch>
2016-05-23 16:11:36 +0200

    New Unit tests with dynamic and non-dynamic Notification

---
+++ Sources/DotNet/WoopsaTest/UnitTestWoopsaDynamicNotification.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Woopsa;
+using System.Diagnostics;
+
+namespace WoopsaTest
+{
+    [TestClass]
+    public class UnitTestWoopsaDynamicNotification
+    {
+        const int QUEUE_SIZE = 1000;
+        const int MONITOR_INTERVAL = 100;
+        const int PUBLISH_INTERVAL = 100;
+
+        [TestMethod]
+        public void TestWoopsaDynamicNotification()
+        {
+            TestObjectServer objectServer = new TestObjectServer();
+            using (WoopsaServer server = new WoopsaServer(objectServer))
+            {
+                // Solution with dynamic client
+                using (dynamic dynamicClient = new WoopsaDynamicClient("http://localhost/woopsa"))
+                {
+
+                    int channel = dynamicClient.SubscriptionService.CreateSubscriptionChannel(QUEUE_SIZE);
+                    // Subscription for a valid variable
+                    dynamicClient.SubscriptionService.RegisterSubscription(channel,
+                       WoopsaValue.WoopsaRelativeLink("/Votes"), TimeSpan.FromMilliseconds(MONITOR_INTERVAL),
+                       TimeSpan.FromMilliseconds(PUBLISH_INTERVAL));
+
+                    // Subscription for an nonexistent variable (just for test)
+                    try
+                    {
+                        dynamicClient.SubscriptionService.RegisterSubscription(channel,
+                            WoopsaValue.WoopsaRelativeLink("/Vote"), TimeSpan.FromMilliseconds(MONITOR_INTERVAL),
+                            TimeSpan.FromMilliseconds(PUBLISH_INTERVAL));
+                        Assert.Fail("Nonexistent variable"); 
+                    }
+                    catch (Exception e)
+                    {
+                        // just catch the exception (normal behaviour)
+                    }
+
+                    WoopsaValue lastNotifications;
+                    WoopsaJsonData jsonData;
+                    int lastNotificationId;
+                    lastNotifications = dynamicClient.SubscriptionService.WaitNotification(channel, 0);
+                    Assert.AreEqual(lastNotifications.Type, WoopsaValueType.JsonData);
+                    jsonData = lastNotifications.JsonData;
+                    lastNotificationId = jsonData[jsonData.Length - 1]["Id"];
+                    Assert.AreEqual(lastNotificationId, 1);
+                    //TODO: voir avec FBG si OK: array vide
+                    //for (int index = 0; index < 10; index++)
+                    //{
+                    //    lastNotifications = dynamicClient.SubscriptionService.WaitNotification(channel, lastNotificationId);
+                    //    Assert.AreEqual(lastNotifications.Type, WoopsaValueType.JsonData);
+                    //    jsonData = lastNotifications.JsonData;
+                    //    Assert.AreEqual(lastNotificationId, index + 2);  // first loop with ID of 2
+                    //    lastNotificationId = jsonData[jsonData.Length - 1]["Id"];
+                    //}
+                    lastNotifications = dynamicClient.SubscriptionService.WaitNotification(channel, 0);
+                    jsonData = lastNotifications.JsonData;
+                    Assert.IsTrue(jsonData.IsArray);
+                    //Assert.IsTrue(jsonData[jsonData.Length - 1].IsDictionary);
+                    //lastNotificationId = jsonData[jsonData.Length - 1]["Id"];
+                    //Assert.AreEqual(lastNotificationId, 1);
+                    //lastNotifications = dynamicClient.SubscriptionService.WaitNotification(channel, lastNotificationId);
+                    jsonData = lastNotifications.JsonData;
+                    Assert.AreEqual(jsonData.Length, 0);
+                }
+            }
+        } //end TestWoopsaDynamicNotification
+    }
+}
---
+++ Sources/DotNet/WoopsaTest/UnitTestWoopsaNotification.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Woopsa;
+using System.Diagnostics;
+
+namespace WoopsaTest
+{
+    [TestClass]
+    public class UnitTestWoopsaNotification
+    {
+
+        [TestMethod]
+        public void TestWoopsaWaitNotification()
+        {
+            TestObjectServer objectServer = new TestObjectServer();
+            using (WoopsaServer server = new WoopsaServer(objectServer))
+            {
+                using (WoopsaClient client = new WoopsaClient("http://localhost/woopsa"))
+                {
+                    int lastNotification = 0;
+                    // Just to show how to see all items
+                    foreach (var item in client.Root.Items)
+                    {
+                        Console.WriteLine("Item = " + item.Name);
+                        if (item.Name == "SubscriptionService")
+                            Console.WriteLine("Trouvé");
+                    }
+
+                    // create a subscription object
+                    WoopsaObject subscription = client.Root.Items.ByNameOrNull("SubscriptionService") as WoopsaObject;
+                    if (subscription != null)
+                    {
+                        int result = 0;
+                        WoopsaMethod methodCreateScubscriptionChannel = subscription.Methods.ByNameOrNull("CreateSubscriptionChannel");
+                        if (methodCreateScubscriptionChannel != null)
+                            // call the method "CreateSubscriptionChannel" on the server
+                            result = methodCreateScubscriptionChannel.Invoke(1000);   // define the queue size
+                        int channel = result;
+
+                        WoopsaMethod methodRegisterScubscription = subscription.Methods.ByNameOrNull("RegisterSubscription");
+                        if (methodRegisterScubscription != null)
+                            // call the method "registerScubscription" on the server
+                            result = methodRegisterScubscription.Invoke(channel, WoopsaValue.WoopsaRelativeLink("/Votes"), 0.2, 0.2);
+                        int subscriptionNbr = result;
+
+                        WoopsaJsonData jData;
+                        WoopsaMethod methodWaitNotification = subscription.Methods.ByNameOrNull("WaitNotification");
+                        if (methodWaitNotification != null)
+                        {
+                            // call the method "WaitNotification" on the server
+                            jData = methodWaitNotification.Invoke(channel, lastNotification).JsonData;
+                            lastNotification = jData[jData.Length - 1]["Id"];
+                            Assert.AreEqual(lastNotification, 1);
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+}
