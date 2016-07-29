a64402c1f6ea2ebaad426ac529870de3a01b83bf
francoisbirling <francois.birling@gmail.com>
2016-07-29 17:02:17 +0200

    Added WoopsaUtils

---
+++ Sources/DotNet/Woopsa/Utils/WoopsaUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Woopsa
+{
+    static public class WoopsaUtils
+    {
+        public static bool IsContextWoopsaThread
+        {
+            get
+            {
+                return IsContextWebServerThread ||
+                    IsContextWoopsaClientSubscriptionThread;
+            }
+        }
+        internal static bool IsContextWebServerThread
+        {
+            get { return WebServer.CurrentWebServer != null; }
+        }
+        internal static bool IsContextWoopsaClientSubscriptionThread
+        {
+            get { return WoopsaClientSubscriptionChannel.CurrentChannel != null; }
+        }
+
+        public static bool IsContextWoopsaTerminatingThread
+        {
+            get
+            {
+                if (WebServer.CurrentWebServer != null)
+                    return WebServer.CurrentWebServer.Aborted;
+                else if (WoopsaClientSubscriptionChannel.CurrentChannel != null)
+                    return WoopsaClientSubscriptionChannel.CurrentChannel.Terminated;
+                else
+                    return false;
+            }
+        }
+    }
+}
