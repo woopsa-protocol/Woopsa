425655f3934f864ff3cc11c601f9293c99cf29aa
francoisbirling <francois.birling@gmail.com>
2016-08-19 11:19:54 +0200

    Extended the RouteHandlerEmbeddedResources so that it can publish the resources of all assemblies. When no assembly is provided, resources from all the loaded assemblies, matching the provided resourcePathInAssembly, are published. When no resourcePathInAssembly is provided, all the resources from all loaded assemblies are published.

--- Sources/DotNet/Woopsa/HTTPServer/RouteHandlers/RouteHandlerEmbeddedResources.cs
+++ Sources/DotNet/Woopsa/HTTPServer/RouteHandlers/RouteHandlerEmbeddedResources.cs
@@ -3,58 +3,54 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Woopsa
 {
     public class RouteHandlerEmbeddedResources : IHTTPRouteHandler
     {
-        private string _baseDirectory;
-        private Assembly _assembly;
-
-        public RouteHandlerEmbeddedResources(string baseDirectory, Assembly assembly)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="resourcePathInAssembly">
+        /// The path where to find resources within the assembly. Empty string "" means the whole assembly.
+        /// HTTP route should contain the remaining path from this origin path to the resource.
+        /// </param>
+        /// <param name="assembly">
+        /// The assembly in which the resources are searched for.
+        /// When assembly is null, the first part of the http route path is used as the assembly name. 
+        /// An assembly with that name is searched in all loaded assemblies.
+        /// </param>
+        public RouteHandlerEmbeddedResources(string resourcePathInAssembly = "", Assembly assembly = null)
         {
-            _baseDirectory = baseDirectory;
+            if (resourcePathInAssembly != string.Empty)
+                _baseDirectory = ResourcePath(resourcePathInAssembly);
+            else
+                _baseDirectory = "";
             _assembly = assembly;
+            _resourceNameByStrippedResourceNameByAssembly =
+                new Dictionary<Assembly, Dictionary<string, string>>();
         }
+
+        #region IHTTPRouteHandler
 
         public void HandleRequest(HTTPRequest request, HTTPResponse response)
         {
-            if (request.Subroute == "")
-            {
-                //If there really is no file requested, then we send a 404!
-                response.WriteError(HTTPStatusCode.NotFound, "Not Found");
-                return;
-            }
-
-            //Find the requested file
-            string[] pathParts = request.Subroute.Split('/');
-            StringBuilder builder = new StringBuilder();
-            for (var i = 0; i < pathParts.Length - 1; i++)
-            {
-                builder.Append(pathParts[i].Replace('-', '_')).Append('/');
-            }
-            builder.Append(pathParts[pathParts.Length - 1]);
-
-            string requestedFile = _baseDirectory + builder.ToString().Replace('/', '.');
-           
-            //Does this file exist?
-            if (HTTPServerUtils.EmbeddedResourceExists(requestedFile, _assembly))
-            {
-                ServeEmbeddedResource(requestedFile, response);
-            }
+            string subRoute = request.Subroute;
+            if (_assembly != null)
+                RespondResource(response, _assembly, subRoute);
             else
             {
-                //Try to get 'index.html', maybe?
-                if ( HTTPServerUtils.EmbeddedResourceExists(requestedFile + "index.html", _assembly) )
+                Assembly assembly;
+                // First element = initial /
+                string[] pathParts = subRoute.Split(new char[] { WoopsaConst.UrlSeparator }, 3);
+                if (pathParts.Length == 3 && pathParts[1] != "")
                 {
-                    ServeEmbeddedResource(requestedFile + "index.html", response);
+                    assembly = AssemblyByName(pathParts[1]);
+                    RespondResource(response, assembly, WoopsaConst.UrlSeparator + pathParts[2]);
                 }
                 else
-                {
-                    response.WriteError(HTTPStatusCode.NotFound, "Not Found");
-                }
+                    response.WriteError(HTTPStatusCode.NotFound,
+                        string.Format("Assembly not Found at '{0}'", subRoute));
             }
         }
 
@@ -63,14 +59,121 @@ namespace Woopsa
             get { return true; }
         }
 
-        private void ServeEmbeddedResource(string filePath, HTTPResponse response)
+        #endregion IHTTPRouteHandler
+
+        const char ResourcePathSeparator = '.';
+
+        private void RespondResource(HTTPResponse response, Assembly assembly, string subRoute)
         {
-            string extension = Path.GetExtension(filePath);
-            response.SetHeader(HTTPHeader.ContentType, MIMETypeMap.GetMIMEType(extension));
-            response.SetHeader(HTTPHeader.LastModified, File.GetLastWriteTime(_assembly.Location).ToHTTPDate());
-            Stream file = _assembly.GetManifestResourceStream(filePath);
-            response.WriteStream(file);
-            file.Close();
+            if (subRoute != "")
+            {
+                string requestedFile = ResourcePath(subRoute);
+                if (_baseDirectory != "")
+                    if (requestedFile != "")
+                        requestedFile = _baseDirectory + ResourcePathSeparator + requestedFile;
+                    else
+                        requestedFile = _baseDirectory;
+                // Does this resource exist?
+                if (!ServeEmbeddedResource(response, assembly, requestedFile))
+                {
+                    string indexHtmlFileResource = requestedFile + ResourcePathSeparator + "index.html";
+                    //Try to get 'index.html', maybe?
+                    if (!ServeEmbeddedResource(response, assembly, indexHtmlFileResource))
+                        response.WriteError(HTTPStatusCode.NotFound,
+                            string.Format("Resource not Found at '{0}'", subRoute));
+                }
+            }
+            else
+                //If there really is no file requested, then we send a 404!
+                response.WriteError(HTTPStatusCode.NotFound,
+                    string.Format("Resource not Found at '{0}'", subRoute));
         }
+
+        private string ResourcePath(string urlPath)
+        {
+            return urlPath.
+                Trim(WoopsaConst.UrlSeparator).
+                Replace(WoopsaConst.UrlSeparator, ResourcePathSeparator);
+        }
+
+        private bool ServeEmbeddedResource(HTTPResponse response, Assembly assembly,
+            string strippedResourceName)
+        {
+            string fullResourceName = FullResourceName(assembly, strippedResourceName);
+            if (!string.IsNullOrEmpty(fullResourceName))
+                try
+                {
+                    using (Stream resourceStream = assembly.GetManifestResourceStream(fullResourceName))
+                        if (resourceStream != null)
+                        {
+                            string extension = Path.GetExtension(strippedResourceName);
+                            response.SetHeader(HTTPHeader.ContentType, MIMETypeMap.GetMIMEType(extension));
+                            response.SetHeader(HTTPHeader.LastModified, File.GetLastWriteTime(assembly.Location).ToHTTPDate());
+                            response.WriteStream(resourceStream);
+                            return true;
+                        }
+                }
+                catch
+                {
+                }
+            return false;
+        }
+
+        private string FullResourceName(Assembly assembly, string strippedResourceName)
+        {
+            Dictionary<string, string> resourceNameByStrippedResourceName;
+            string result;
+            if (!_resourceNameByStrippedResourceNameByAssembly.TryGetValue(
+                    assembly, out resourceNameByStrippedResourceName))
+            {
+                resourceNameByStrippedResourceName = new Dictionary<string, string>();
+                _resourceNameByStrippedResourceNameByAssembly[assembly] =
+                    resourceNameByStrippedResourceName;
+            }
+            if (!resourceNameByStrippedResourceName.TryGetValue(strippedResourceName, out result))
+            {
+                if (resourceNameByStrippedResourceName.Count == 0)
+                    // Populate the dictionary
+                    foreach (var item in assembly.GetManifestResourceNames())
+                    {
+                        string[] resourceNameParts = item.Split(new char[] { ResourcePathSeparator }, 2);
+                        if (resourceNameParts.Length == 2)
+                        {
+                            resourceNameByStrippedResourceName[resourceNameParts[1]] = item;
+                            if (resourceNameParts[1] == strippedResourceName)
+                                result = item;
+                        }
+                    }
+            }
+            return result;
+        }
+
+        private Assembly AssemblyByName(string name)
+        {
+            Assembly result;
+
+            if (_assemblyByName == null)
+            {
+                _assemblyByName = new Dictionary<string, Assembly>();
+                result = null;
+            }
+            else
+                _assemblyByName.TryGetValue(name, out result);
+            if (result == null)
+            {
+                result = AppDomain.CurrentDomain.GetAssemblies().
+                      SingleOrDefault(assembly => assembly.GetName().Name == name);
+                if (result != null)
+                    _assemblyByName[result.GetName().Name] = result;
+            }
+            return result;
+        }
+
+        private string _baseDirectory;
+        private Assembly _assembly;
+        private Dictionary<string, Assembly> _assemblyByName;
+        private Dictionary<Assembly, Dictionary<string, string>>
+            _resourceNameByStrippedResourceNameByAssembly;
     }
+
 }
