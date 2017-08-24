3cede6557605d168bcbc48595d0cf96f0bed3483
François Birling <francois.birling@gmail.com>
2017-08-24 10:50:29 +0200

    Change in BaseAuthenticator (with minor compatibility break).
    A parameter "request" of type HTTPRequest has been added to the Authenticate method to permit selective access to resources.
    In addition, the parameters username and password can be null in the method Authenticate, if no authorization has been received in http header. So, it is possible for the Authenticator to permit access to certain resources without any authorization information.

--- Sources/DotNet/Woopsa/HTTPServer/Processors/WWWAuthenticator.cs
+++ Sources/DotNet/Woopsa/HTTPServer/Processors/WWWAuthenticator.cs
@@ -17,13 +17,15 @@ namespace Woopsa
 
         public BaseAuthenticator(string realm)
         {
-            Realm = realm;            
+            Realm = realm;
         }
 
         public string Realm { get; private set; }
 
         public virtual bool Process(HTTPRequest request, HTTPResponse response)
         {
+            string username;
+            string password;
             bool authenticated;
             _currentUserName = null;
             if (request.Headers.ContainsKey(HTTPHeader.Authorization))
@@ -31,32 +33,44 @@ namespace Woopsa
                 string authString = request.Headers[HTTPHeader.Authorization].Split(' ')[1];
                 authString = Encoding.GetEncoding("ISO-8859-1").GetString(Convert.FromBase64String(authString));
                 string[] parts = authString.Split(':');
-                string username = parts[0];
-                string password = parts[1];
-                authenticated = Authenticate(username, password);
-                if (authenticated)
-                    _currentUserName = username;
+                username = parts[0];
+                password = parts[1];
             }
             else
-                authenticated = false;
-            if (!authenticated)
-            { 
+            {
+                username = null;
+                password = null;
+            }
+            authenticated = Authenticate(request, username, password);
+            if (authenticated)
+                _currentUserName = username;
+            else
+            {
                 response.SetHeader(HTTPHeader.WWWAuthenticate, "Basic Realm=\"" + Realm + "\"");
                 response.WriteError(HTTPStatusCode.Unauthorized, "Unauthorized");
             }
             return authenticated;
         }
 
-        protected abstract bool Authenticate(string username, string password);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username">null if no Authorization received in HTTP headers</param>
+        /// <param name="password">null if no Authorization received in HTTP headers</param>
+        /// <returns></returns>
+        protected abstract bool Authenticate(HTTPRequest request, string username, string password);
+
     }
 
     public class AuthenticationCheckEventArgs : EventArgs
     {
-        public AuthenticationCheckEventArgs(string username, string password)
+        public AuthenticationCheckEventArgs(HTTPRequest request, string username, string password)
         {
+            Request = request;
             Username = username;
             Password = password;
         }
+        public HTTPRequest Request { get; private set; }
         public string Username { get; private set; }
         public string Password { get; private set; }
 
@@ -67,7 +81,7 @@ namespace Woopsa
 
     public class SimpleAuthenticator : BaseAuthenticator
     {
-        public SimpleAuthenticator(string realm, AuthenticationCheck authenticationCheck): 
+        public SimpleAuthenticator(string realm, AuthenticationCheck authenticationCheck) :
             base(realm)
         {
             if (authenticationCheck != null)
@@ -76,9 +90,10 @@ namespace Woopsa
                 throw new NotImplementedException("No DoCheck delegate was specified for WWWAuthenticator.");
         }
 
-        protected override bool Authenticate(string username, string password)
+        protected override bool Authenticate(HTTPRequest request, string username, string password)
         {
-            AuthenticationCheckEventArgs eventArgs = new AuthenticationCheckEventArgs(username, password);
+            AuthenticationCheckEventArgs eventArgs = new AuthenticationCheckEventArgs(request, 
+                username, password);
             eventArgs.IsAuthenticated = false;
             AuthenticationCheck(this, eventArgs);
             return eventArgs.IsAuthenticated;
