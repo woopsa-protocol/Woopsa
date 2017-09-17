d82cd631010a97dd32628f4093154f3ed62110c4
fabien-chevalley <fabien-chevalley@users.noreply.github.com>
2017-09-17 12:56:20 +0200

    remove dead code

--- Sources/DotNet/Woopsa/Utils/JsonSerializer.cs
+++ Sources/DotNet/Woopsa/Utils/JsonSerializer.cs
@@ -24,7 +24,6 @@ namespace Woopsa
             return null;
         }
 
-
         public static object[] ToArray(object obj)
         {
             var jarray = obj as JArray;
@@ -33,62 +32,10 @@ namespace Woopsa
 
             return null;
         }
-
-        //public object Deserialize(string json)
-        //{
-        //    var dictionnary = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
-        //    if (dictionnary != null)
-        //        return dictionnary;
-
-        //    var array = JsonConvert.DeserializeObject<object[]>(json);
-        //    if (array != null)
-        //        return array;
-
-        //    return JsonConvert.DeserializeObject(json);
-        //}
-
-
+        
         public static T Deserialize<T>(string json)
         {
             return JsonConvert.DeserializeObject<T>(json);
         }
-
-        //public string Serialize(object obj)
-        //{
-        //    var serializer = new DataContractJsonSerializer(obj.GetType());
-        //    string json;
-        //    using (var stream = new MemoryStream())
-        //    {
-        //        serializer.WriteObject(stream, obj);
-        //        json = Encoding.UTF8.GetString(stream.ToArray());
-        //    }
-
-        //    return json;
-        //}
-
-        //public T Deserialize<T>(string json)
-        //{
-        //    object obj;
-        //    using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
-        //    {
-        //        DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
-
-        //        obj = serializer.ReadObject(stream);
-        //    }
-        //    return (T)obj;
-        //}
-
-
-        //public object Deserialize(string json)
-        //{
-        //    object obj;
-        //    using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
-        //    {
-        //        DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Dictionary<string, object>));
-
-        //        obj = serializer.ReadObject(stream);
-        //    }
-        //    return obj;
-        //}
     }
 }
