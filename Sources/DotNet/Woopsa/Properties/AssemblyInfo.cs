1a1e482fb2d81bb52f1895e3731ada0e8c520962
Paul-Henri Maillefer <paul-henri.maillefer@objectis.ch>
2020-08-31 22:14:00 +0200

    Unit test that show the blocking case and the memory leaks
    Before there was 2 subscriptions in the client channel instead of only 1 at the end.

--- Sources/DotNet/Woopsa/Properties/AssemblyInfo.cs
+++ Sources/DotNet/Woopsa/Properties/AssemblyInfo.cs
@@ -1,5 +1,8 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 
 [assembly: ComVisible(false)]
 [assembly: Guid("11e39986-1641-4ffa-b91c-a928a72b819e")]
+
+[assembly: InternalsVisibleTo("WoopsaTest")]
