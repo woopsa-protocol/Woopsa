c46efefc4794cb651a78cab30895d5d8d464d796
Paul-Henri Maillefer <paul-henri.maillefer@objectis.ch>
2020-12-08 11:07:30 +0100

    Clean projects settings

--- Sources/DotNet/WoopsaDemoClient/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("WoopsaDemoClient")]
-[assembly: AssemblyDescription("Client demonstration for woopsa .net")]
-[assembly: ComVisible(false)]
-[assembly: Guid("f2bd77d7-5671-4d9b-94ab-8c1a5c16168b")]
