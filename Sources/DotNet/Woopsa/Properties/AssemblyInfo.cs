b9a8d9a913aceb55684a0e9e6a410acc805f9566
Paul-Henri Maillefer <paul-henri.maillefer@objectis.ch>
2020-12-08 11:12:42 +0100

    Remove properties folders

--- Sources/DotNet/Woopsa/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-
-[assembly: ComVisible(false)]
-[assembly: Guid("11e39986-1641-4ffa-b91c-a928a72b819e")]
-
-[assembly: InternalsVisibleTo("WoopsaTest")]
